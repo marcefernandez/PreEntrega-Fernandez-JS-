e913e461a99eaedef9a788edf521b6d48adb80ca
marcefernandez <sebastianmf2014@hotmail.com>
2023-01-03 12:48:30 -0300

    Tercer preEntrega JS

---
+++ src/components/stock.js
@@ -0,0 +1,64 @@
+const productos = 
+[
+    {
+        id: 1,
+        nombre: "Piercing para la oreja",
+        precio: 500,
+        img: 'public/img/foto1.jpg',
+        cantidad:1,
+    },
+
+    {
+        id: 2,
+        nombre:"Piercing para la nariz",
+        precio: 700,
+        img: 'public/img/foto2.jpg',
+        cantidad:1,
+    },
+
+    {
+        id: 3,
+        nombre:"Piercing para cejas",
+        precio: 700,
+        img: 'public/img/foto1.jpg',
+        cantidad:1,
+    },
+
+    {
+        id: 4,
+        nombre: "Piercing de ombligo",
+        precio: 1000,
+        img: 'public/img/foto4.jpg',
+        cantidad:1,
+    },
+
+    {
+        id: 5,
+        nombre: "Piercing de pezón",
+        precio: 1000,
+        img: 'public/img/foto5.jpg',
+        cantidad:1,
+    },
+
+    {   id: 6,
+        nombre: "Barra de oreja",
+        precio: 1500,
+        img: 'public/img/foto6.jpg',
+        cantidad:1,
+    },
+
+    {
+        id: 7,
+        nombre: "Piercing de labios",
+        precio: 1000,
+        img: 'public/img/foto5.jpg',
+        cantidad:1,
+    },
+
+    {   id: 8,
+        nombre: "Piercing colgante",
+        precio: 1500,
+        img: 'public/img/foto6.jpg',
+        cantidad:1,
+    }
+];
